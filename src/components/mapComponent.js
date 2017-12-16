497b7d8968dfe1d01385a7600df6fe7b3a64dd18
maolinzuo <maolin.zuo94@gmail.com>
2017-12-16 01:48:46 -0500

    add map

---
+++ src/components/mapComponent.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { compose } from "recompose";
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker,
+  InfoWindow
+} from "react-google-maps";
+
+const MapComponent = compose(
+    withScriptjs,
+    withGoogleMap
+  )(props => (
+    <GoogleMap defaultZoom={13} defaultCenter={{ lat: 30.2672, lng: -97.7431 }}>
+      {props.markers.map((marker, index) => {
+        return(
+          <div key={index}>
+            <Marker
+                  {...marker}>
+            </Marker>
+          </div>
+        )
+      
+      })}
+    </GoogleMap>
+  
+  ));
+  
+export default MapComponent;
---
+++ src/components/mapWrapper.js
@@ -0,0 +1,37 @@
+import _ from "lodash";
+import React, { Component } from "react";
+import MapComponent from './mapComponent';
+
+const URL_GOOGLE_API = "https://maps.googleapis.com/maps/api/js?key=AIzaSyCHGt6967ispOV3GgP_sG_np-07B9yYtBU&v=3.exp&libraries=geometry,drawing,places";
+
+class Map extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            markers : props.markers
+        }
+    }
+
+    componentWillReceiveProps(nextProps) {
+        this.setState({ markers: nextProps.markers }); 
+    }
+
+
+    render() {
+        return (
+          <div>
+              <MapComponent
+              googleMapURL={URL_GOOGLE_API}
+              loadingElement={<div style={{ height: `100%` }} />}
+              containerElement={<div style={{ height: `600px` }} />}
+              mapElement={<div style={{ height: `100%` }} />}
+              markers={this.state.markers}
+              onMarkerClick={this.handleMarkerClick}
+              onMarkerClose={this.handleMarkerClose}/>
+          </div>
+        )
+      }
+
+}
+
+export default Map;
